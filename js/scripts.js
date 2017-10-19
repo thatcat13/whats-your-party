e09b166fe44f0be9adda0a4f7da8c7164b7470fb
Cat Janowitz and Griffin P Longtin <griffin@>
2017-10-19 11:34:54 -0700

    add lib and con if else statements to check numbers and output according to party

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,48 @@ party.prototype.conCounter = function() {
 
 $(function() {
   var catParty = new party();
-  $("form button").submit(function(event) {
+  catParty.lib = 0;
+  catParty.con = 0;
+  $("form button").click(function(event) {
     event.preventDefault();
+    var x1 = $("select#x").val();
+    var y1 = $("select#y").val();
+    var z1 = $("select#z").val();
+    var bla1 = $("select#bla").val();
 
-  
+    if (x1 === "lib") {
+      catParty.libCounter();
+    } else {
+      catParty.conCounter();
+    }
+
+    if (y1 === "lib") {
+      catParty.libCounter();
+    } else {
+      catParty.conCounter();
+    }
+
+    if (z1 === "lib") {
+      catParty.libCounter();
+    } else {
+      catParty.conCounter();
+    }
+
+    if (bla1 === "lib") {
+      catParty.libCounter();
+    } else {
+      catParty.conCounter();
+    }
+
+    if (catParty.lib === catParty.con) {
+      $("#germany-tab").show();
+    } else if (catParty.con > catParty.lib) {
+      $("#conserv-tab").show();
+    } else if (catParty.con < catParty.lib) {
+      $("#liberal-tab").show();
+    }
+
+
   });
 
 
